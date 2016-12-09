2c538bb50ffa767c6e1cca7ad34632c390c1ce0b
Getro Naissance <getro89@gmail.com>
2016-12-09 10:39:03 -0800

    added save method and update method, with the specs successfully passing

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,19 +1,32 @@
 class Stylist
-  attr_reader(:name, :id)
+
+  attr_reader(:id, :name)
 
   define_method(:initialize) do |attributes|
-    @name = attributes.fetch(:name)
     @id = attributes.fetch(:id)
+    @name = attributes.fetch(:name)
   end
 
   define_singleton_method(:all) do
     returned_stylists = DB.exec("SELECT * FROM stylists;")
-    stylist = []
-    returned_stylists.each do |stylist|
-      name = stylist.fetch('name')
-      id = stylist.fetch('id').to_i
-      stylist.push(Stylist.new({:name => name, :id => id}))
+    stylis = []
+    returned_stylists.each() do |styl|
+      id = styl.fetch('id').to_i
+      name = styl.fetch('name')
+      stylis.push(Stylist.new({:id => id, :name => name}))
     end
-    stylist
+    stylis
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i
+  end
+
+  define_method(:==) do |other|
+    self.name() == other.name() && self.id() == other.id()
+  end
+
+
+
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,24 +1,49 @@
 require('spec_helper')
 
 describe(Stylist) do
-  describe('#name') do
-    it('will return the name of the client') do
-      test_stylist = Stylist.new({:name => "josh", :id => nil})
-      expect(test_stylist.name()).to(eq("josh"))
-    end
-  end
-
-  # describe('#id') do
-  #   it('will return the id of the client') do
-  #     test_stylist = Stylist.new({:name => "james", :id => nil })
-  #     expect(test_stylist.id()).to(be_an_instance_of(Fixnum))
-  #   end
-  # end
 
   describe('.all') do
     it('will return an empty array at first because we have not saved anything') do
-      test_stylist = Stylist.new({:name => "josh", :id => nil})
       expect(Stylist.all()).to(eq([]))
     end
+  end
+
+  describe('#id') do
+    it('will return the id of the stylist once it is saved') do
+      test_stylist = Stylist.new({:id => nil, :name => 'james'})
+      test_stylist.save()
+      expect(test_stylist.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#name') do
+    it('will return the name of the stylist') do
+      test_stylist = Stylist.new({:id => nil, :name => "josh"})
+      expect(test_stylist.name()).to(eq('josh'))
+    end
+  end
+
+  describe('#save') do
+    it('will save the new instance of the stylist class into the table') do
+      test_stylist = Stylist.new({:id => nil, :name => "Getro"})
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
+  describe('#==') do
+    it('will overide the == method') do
+      test_stylist = Stylist.new({:id => nil, :name => "bryan"})
+      test_stylist2 = Stylist.new({:id => nil, :name => "bryan"})
+      expect(test_stylist).to(eq(test_stylist2))
+    end
+  end
+
+  describe('#update') do
+    it('will update the stylist') do
+      test_stylist = Stylist.new({:id => nil, :name => "bowl"})
+      test_stylist.save()
+      test_stylist.update({:name => "Ben"})
+      expect(test_stylist.name())to(eq("Ben"))
   end
 end
